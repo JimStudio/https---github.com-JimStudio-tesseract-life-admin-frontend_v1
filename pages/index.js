4edcd052338ff53222da5a82b883124abd01372e
JimStudio <chuixiaojian@hotmail.com>
2022-08-12 11:07:28 +0800

    new endit

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,41 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div className='container'>
       <Head>
          <title>Work Flow</title>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
+         integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"></link>
        </Head>
-    <img
-      className="mx-auto h-12 w-auto"
-      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-      alt="Workflow"
-    />
-    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-      Sign in to your account
-    </h2>
-    <div>
-    <input id="email-address" name="email" type="email"focus:outline-none 
-           placeholder="Email address" value="user@gmail.com"></input>
-    </div>
-    <div>
-    <input id="email-address" name="email" type="email"focus:outline-none 
-           placeholder="Email address" value="user@gmail.com"></input>
-    </div>
-    <div>
-       <input id="remember-me" name="remember-me" type="checkbox"></input>
-       <label for="remember-me">Remember me</label>
-       <a href="#">Forgot your password?</a>
-    </div>
-  </div>
-  )
+        <img
+             className="rounded mx-auto d-block w-25 p-3 mt-5"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+               alt="Workflow"/>
+                <h2 className="mb-5 text-center font-extrabold text-gray-900">Sign in to your account</h2>
+    
+                <form className='form-inline w-50 mx-auto'>
+                  <div className="form-group align-items-center">
+                    <input id="email-address" name="email" type="email" placeholder="Email address"  className="form-control " 
+                      aria-label="Large"></input>
+                      </div>
+                          <div className="form-group">
+                             <input id="password" name="password" type="password" placeholder="Password"  className="form-control col-sm-10" 
+                               aria-label="Large"></input>
+         </div>
+         <div className='container row'>
+             <div className="form-check col-md-5">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+              <label className="form-check-label">Check me out</label>
+         </div>
+         <div className="form-check col-md-7 ">
+         <a href="#" className="link-primary "> Forgot your password?</a>
+         </div>
+         </div>
+             <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+        
+        </div>
+       )
 }
